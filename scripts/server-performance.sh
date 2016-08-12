08ae2fd8d232e0585a9f3008cfcb058f676ecf08
Daren Welsh <darenwelsh@gmail.com>
2016-08-12 12:39:21 -0500

    init

---
+++ scripts/server-performance.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# server-performance.sh
+#
+# Send an announcement to Slack reporting server performance
+#
+
+if [ "$(whoami)" != "root" ]; then
+    echo "Try running this script with sudo: \"sudo server-performance.sh\""
+    exit 1
+fi
+
+# Use this webhook to post to slack channel
+slackwebhook="<webhook>"
+
+# get all the dataz
+topdata=$(top -b -n 1)
+
+topheader=$(echo "$topdata" | grep "load average")
+# top - 15:36:28 up 48 days, 21:35,  1 user,  load average: 0.08, 0.16, 0.21
+# remove "top - "
+topheader=${topheader#top - }
+
+topmemory=$(echo "$topdata" | grep "KiB Mem")
+# KiB Mem : 16269056 total,   275304 free,  2177008 used, 13816744 buff/cache
+topmemory2=$(echo "$topdata" | grep "avail Mem")
+# KiB Swap:  3907580 total,  3655212 free,   252368 used.  4522976 avail Mem
+
+# isolate total memory value
+totalmemory=${topmemory#*KiB Mem : }
+totalmemory=${totalmemory% total*}
+
+# isolate available memory value
+availmemory=${topmemory2#*used. }
+availmemory=${availmemory% avail Mem*}
+
+# calculate memory used
+((memoryused = $totalmemory - $availmemory))
+
+# calculate percent of total memory used
+memorypercentused=$(echo print 100*${memoryused}/${totalmemory}. | python )
+memorypercentused=$(printf "%0.1f\n" $memorypercentused)
+# format a rounded version for use in comparison later
+memorypercentusedrounded=$(printf "%.0f\n" $memorypercentused)
+
+# Individual tasks
+# PID user priority nice virtual resident shareable CPU% memory% time command
+topmysql=$(echo "$topdata" | grep "mysql")
+# 14625 mysql     20   0 1813624 516236   7796 S   0.0  3.2  25:57.82 mysqld
+topelastic=$(echo "$topdata" | grep "elastic")
+# 15063 elastic+  20   0 3741348 583332  18516 S   0.0  3.6  41:58.35 java
+topmemcached=$(echo "$topdata" | grep "memcach")
+# 14264 memcach+  20   0  403708  91804    776 S   0.0  0.6   0:43.09 memcached
+topparsoid=$(echo "$topdata" | grep "parsoid")
+# 21245 parsoid   20   0  698232  67836   7956 S   0.0  0.4   0:00.91 node
+topapache=$(echo "$topdata" | grep "apache")
+# 11925 apache    20   0  706516  77052  50568 S   0.0  0.5   0:06.94 httpd
+
+# calculate total memory% used by all tasks of each application
+mysqltotalmem=$(echo "$topdata" | grep "mysql" | awk '{ sum += $10 } END { print sum }')
+elastictotalmem=$(echo "$topdata" | grep "elastic" | awk '{ sum += $10 } END { print sum }')
+memcachedtotalmem=$(echo "$topdata" | grep "memcach" | awk '{ sum += $10 } END { print sum }')
+parsoidtotalmem=$(echo "$topdata" | grep "parsoid" | awk '{ sum += $10 } END { print sum }')
+apachetotalmem=$(echo "$topdata" | grep "apache" | awk '{ sum += $10 } END { print sum }')
+
+jsontitle="Performance Report"
+
+# at what level do we display amber color
+warninglevel="50"
+# at what level to we display red color
+dangerlevel="75"
+jsoncolor="good"
+memoryusedtext="Mem: ${memorypercentused}%"
+alerttext=""
+if [ "$memorypercentusedrounded" -gt "$warninglevel" ]; then
+    jsoncolor="warning"
+    # bold the memory text
+    memoryusedtext="*Mem: ${memorypercentused}%*"
+fi
+if [ "$memorypercentusedrounded" -gt "$dangerlevel" ]; then
+    jsoncolor="danger"
+    # alert the channel
+    alerttext="\n<!everyone> "
+fi
+
+report="${topheader} ${alerttext}${memoryusedtext}\nMySQL: ${mysqltotalmem}% ES: ${elastictotalmem}% Memcached: ${memcachedtotalmem}% Parsoid: ${parsoidtotalmem}% Apache: ${apachetotalmem}%"
+
+# Option 1: Manually create json instead of using meza script
+
+json="{
+      \"attachments\": [
+          {
+              \"color\": \"${jsoncolor}\",
+              \"fallback\": \"${memorypercentused}%\",
+              \"text\": \"${report}\",
+              \"mrkdwn_in\": [\"text\"]
+          }
+      ]
+  }"
+
+curl -s -d "payload=$json" "$slackwebhook"
+
